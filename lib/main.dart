4b4405a7766c8d5e4151df539f74c9c0a63f3d84
txhuy1001 <huy.truong.it1001@gmail.com>
2022-05-20 23:07:30 +0700

    first commit for running app

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      theme: ThemeData(primaryColor: Colors.lightBlueAccent),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+
+    return SafeArea(
+      top: true,
+      bottom: true,
+      child: Stack(
+        children: [
+
+          Scaffold(
+            body: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(
+                  width: size.width * 0.3,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Row(
+                          children: [
+                            Expanded( flex: 1,child: Container(color: Colors.grey)),
+                            Expanded(flex: 2,child: Container(color: Colors.blue)),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Row(
+                          children: [
+                            Expanded(flex: 1,child: Container(color: Colors.orange)),
+                            Expanded(flex: 2,child: Container(color: Colors.blue)),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Row(
+                          children: [
+                            Expanded(child: Container(color: Colors.blueAccent)),
+                            Expanded(child: Container(color: Colors.blue)),
+                            Expanded(child: Container(color: Colors.blue)),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Row(
+                          children: [
+                            Expanded(child: Container(color: Colors.redAccent)),
+                            Expanded(child: Container(color: Colors.green)),
+                            Expanded(child: Container(color: Colors.yellow)),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: size.height * 0.25,
+                        color: Colors.black,
+                      ),
+                      Container(
+                        height: size.height * 0.25,
+                        color: Colors.yellowAccent,
+                      ),
+                      Container(
+                        height: size.height * 0.3,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: size.width * 0.43,
+                  color: Colors.pinkAccent,
+                ),
+                SizedBox(
+                  width: size.width * 0.02,
+                ),
+                Container(
+                  width: size.width * 0.25,
+                  color: Colors.pinkAccent,
+                ),
+                SizedBox(
+
+                  child: Stack(
+                    children: [
+                      Container(
+                        color: Colors.black,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: const FractionalOffset(0.3, 0.58),
+            // heightFactor: 7.0,
+            // widthFactor: 3.0,
+            child: Container(
+              width: size.width*0.22,
+              height: size.height*0.15,
+              color: Colors.black54,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            // heightFactor: 7.0,
+            // widthFactor: 3.0,
+            child: FloatingActionButton(
+              onPressed: (){},
+              child: const Icon(Icons.home),
+            )
+          ),
+        ],
+      ),
+    );
+  }
+}
